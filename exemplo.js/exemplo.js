08099b99de90149bfa2d2592c09877aac5c253c6
flavia <flaviafgl@hotmail.com>
2018-01-23 22:54:18 -0200

    ajustes em aula

---
+++ exemplo.js/exemplo.js
@@ -0,0 +1,6 @@
+let botao = document.QuerySelector('button');
+let subtitulo = document.QuerySelector('h2');
+
+function CorTexto() {
+subtitulo.stule.color = 'red';
+}
--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let titulo = document.querySelector('h1');
 let texto = document.querySelector('p');
 
 function mudarTitulo() {
-  titulo.innerHTML = 'Sua cidade';
+  titulo.innerHTML = 'America do Norte';
 }
 
 function mudarTexto() {
   texto.style.color = 'red';
+  texto.style.backgroundColor = 'blue';
 }
